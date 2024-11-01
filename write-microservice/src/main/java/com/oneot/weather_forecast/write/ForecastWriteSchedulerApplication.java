337df294ee2c7f422abd39784f5ebd8876cfc681
Olayiwola <olayiwola@gmail.com>
2024-11-01 17:12:54 +0100

    "configured dev live reload server"

--- write-microservice/src/main/java/com/oneot/weather_forecast/write/ForecastWriteSchedulerApplication.java
+++ write-microservice/src/main/java/com/oneot/weather_forecast/write/ForecastWriteSchedulerApplication.java
@@ -2,7 +2,7 @@ package com.oneot.weather_forecast.write;
 
  import com.oneot.weather_forecast.write.config.WeatherApiProperties;
  import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+ import org.springframework.boot.autoconfigure.SpringBootApplication;
  import org.springframework.boot.autoconfigure.domain.EntityScan;
  import org.springframework.boot.context.properties.EnableConfigurationProperties;
  import org.springframework.context.annotation.ComponentScan;
